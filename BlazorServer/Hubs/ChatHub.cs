9a4ccf9785e47fd73aea1dd699f7344fad9657f7
Andrei Costache <costacheandrei43@gmail.com>
2023-02-02 21:23:23 +0200

    Added project

---
+++ BlazorServer/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServer.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public Task SendMessageToCaller(string user, string message)
+    {
+        return Clients.Caller.SendAsync("ReceiveMessage", user, message);
+    }
+}
---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfClient;
+
+public partial class MainWindow : Window
+{
+
+    HubConnection connection;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7140/chathub")
+            .WithAutomaticReconnect()
+            .Build();
+
+        connection.Reconnecting += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Attempting to reconnect...";
+                messages.Items.Add(newMessage);
+            });
+
+            return Task.CompletedTask;
+        };
+
+        connection.Reconnected += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Reconnected to the server";
+                messages.Items.Clear();
+                messages.Items.Add(newMessage);
+            });
+
+            return Task.CompletedTask;
+        };
+
+        connection.Closed += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Connection closed";
+                messages.Items.Add(newMessage);
+                openConnection.IsEnabled = true;
+                sendMessage.IsEnabled = false;
+            });
+
+            return Task.CompletedTask;
+        };
+    }
+
+    private async void openConnection_Click(object sender, RoutedEventArgs e)
+    {
+
+        // cand clientul a primit un mesaj de tipul ReceiveMessage de la server, il handleuieste
+        connection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = $"{user}: {message}";
+                messages.Items.Add(newMessage);
+            });
+        });
+
+        try
+        {
+            await connection.StartAsync();
+            messages.Items.Add("Connection Started");
+            openConnection.IsEnabled = false;
+            sendPrivateMessage.IsEnabled = true;
+            sendMessage.IsEnabled = true;
+        }
+        catch(Exception ex)
+        {
+            messages.Items.Add($"{ex.Message}");
+        }
+    }
+
+    private async void sendMessage_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await connection.InvokeAsync("SendMessage", "WPF Client", messageInput.Text); 
+        }
+        catch(Exception ex )
+        {
+            messages.Items.Add($"{ex.Message}");
+
+        }
+    }
+
+    private async void sendPrivateMessage_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            await connection.InvokeAsync("SendMessageToCaller", "WPF Client", messageInput.Text);
+        }
+        catch (Exception ex)
+        {
+            messages.Items.Add($"{ex.Message}");
+
+        }
+    }
+}
